6b4851dad79519a562f443df5a608ee3d97738ae
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-05-29 21:36:09 +0530

    user model

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,5 +1,11 @@
-export const register = (req, res, next) => {
+import User from "../models/User";
+export const register = async (req, res, next) => {
   try {
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: req.body.password,
+    });
   } catch (err) {
     next(err);
   }
