a7ee6f2f4e435c167102be2c5deb9429e8cc9d4f
L4KsHM1 <lakshmishajan.me@gmail.com>
2024-05-31 23:06:11 +0530

    usercontroller

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,16 @@
 import express from "express";
 
 export const userRouter = express.Router();
+
+//CREATE
+Router.post("/", createUser);
+//UPDATE
+userRouter.put("/:id", updateUser);
+//DELETE
+userRouter.delete("/:id", deleteUser);
+
+//GET
+userRouter.get("/:id", getUser);
+//GET ALL
+
+userRouter.get("/", getUser);
